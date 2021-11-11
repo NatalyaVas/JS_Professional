919e8c85c902aa4c1518024d280de33e266602d2
Your Name <kirnata@mail.ru>
2021-11-11 17:38:18 +0300

    Added cart

--- lesson_2/js/main.js
+++ lesson_2/js/main.js
@@ -1,65 +1,47 @@
-class Cart {
-    constructor() {
-        this.getAllProducts(); //Суммировать все продукты в корзине.
-        this.getSumCart(); //Суммировать цены в корзине.
-    }
-}
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-class CartItem {
-    constructor() {
-        
-    }
-}
-
-
-class ProductList { //2.При создании объекта вызывается конструктор
-    constructor(container='.products') { //Конструктор имеет параметры по умолчанию. В div c селектором products нужно вставить верстку.
-        this.container = container; //3. Создаем глобальные переменные в нашем классе.
-        this.goods = []; //Пока пустой массив товаров
-        this._fetchProducts(); //4.Вызываем в текущем классе(нижнее подчеркивание) метод fetch.
-        this.render(); //5.Вызываем метод вывода товаров на страницу.
-        this.getSum(); //Вызываем метод расчета суммы
+class ProductList {
+    constructor(container='.products') {
+        this.container = container; 
+        this.goods = [];//массив товаров из JSON-документа
+        this._getProducts() //После получения массива запускается then.
+            .then(data => { //data - объект js из массивов.
+                this.goods = data; // Записываем массив в goods.
+                // console.log(data);
+                this.render() //Запускаем render, который выведет наш массив на страничку
+            });
+        // this.getSum();
     }
 
-    _fetchProducts() { //4.Вызов метода
-        this.goods = [ //Обращаемся к нашему массиву goods, наполняем его товарами.
-            {id: 1, title: 'Notebook', price: 2000},
-            {id: 2, title: 'Mouse', price: 20},
-            {id: 3, title: 'Keyboard', price: 200},
-            {id: 4, title: 'Gamepad', price: 50},
-        ];
+    _getProducts(){ //_ - это нестрогая инкапсуляция.
+
+        return fetch(`${API}/catalogData.json`) //путь до файла, из которого парсим данные. Часть пути сохранили в const. Получаем промис.
+            .then(result => result.json()) //C помощью метода json мы получаем объект JS и вернем  промис. При успешном коннекте мы получаем массив объектов. 
+            .catch(error => { //Если коннект не прошел, то сообщим об ошибке
+                console.log(error);
+            })
     }
 
     getSum() {
-        // let s = 0; //Сумма начинается с нуля.
-        // this.goods.forEach(item => { //Массив обходим циклом forEach, item - каждый.
-        //     s += item.price; //Из каждого элемента берем прайс и суммируем
-        // })
-
-        // for(let product of this.goods) {
-        //     s += product.price;
-        // }
-
-        let res = this.goods.reduce((sum, item) => sum + item.price, 0); //0 - начальное значение первого нашего аргумента sum. item - каждый элемент массива.
+        let res = this.goods.reduce((sum, item) => sum + item.price, 0); 
         alert(res);
     }
 
-    render() { //5. Вызываем метод
-        const block = document.querySelector(this.container); //Ищем элемент верстки, у кот. селектор this.container. Это наш .products. Записали в переменную block.
-        for(let product of this.goods) { //Обходим каждый товар нашего массива goods.
-            const item = new ProductItem(product); //6.Создаем для каждого товара верстку, для этого создаем новый объект. В конструктор класса ProductItem передаем объект с товаром product.
-            block.insertAdjacentHTML('beforeend', item.render()); //8. Наполняем наш товар версткой. Перед окончанием нашего блока мы вставим то, что вернет метод render.
-
+    render() { 
+        const block = document.querySelector(this.container); 
+        for(let product of this.goods) { 
+            const item = new ProductItem(product); 
+            block.insertAdjacentHTML('beforeend', item.render());
         }
     }
 }
 
 class ProductItem {
-    constructor(product, img='img/prod_1.png') { // 7. Конструктор принимает объект товар и у всех товаров общая картинка. Наполняем наш товар свойствами. Извлекаем title, id...
+    constructor(product, img='img/prod_1.png') { 
         this.title = product.title;
         this.id = product.id;
         this.price = product.price;
-        this.img = img; //Картинку берем из параметра по умолчания конструктора.
+        this.img = img;
     }
     render() {
         return `<div class="product-item">
@@ -72,27 +54,71 @@ class ProductItem {
 }
 
 
-let list = new ProductList(); //1.С помощью new оздаем объект класса ProductList
+let list = new ProductList();
 
+// class Cart {
+//     addGoods() {
+        
+//     }
+//     removeGoods(){
 
-// const products = [
-//     { id: 1, title: 'Notebook', price: 2000, img: 'img/prod_1.png' },
-//     { id: 2, title: 'Mouse', price: 20, img: 'img/prod_1.png' },
-//     { id: 3, title: 'Keyboard', price: 200, img: 'img/prod_1.png' },
-//     { id: 4, title: 'Gamepad', price: 50, img: 'img/prod_1.png' },
-// ];
-// const renderProduct = (param) => {
-//     return `<div class="product-item">
-//                 <h3>${param.title}</h3>
-//                 <img src = ${param.img}>
-//                 <p>${param.price}</p>
-//                 <button class="buy-btn">Купить</button>
-//             </div>`
-// }; //4.Принимаем название товара, стоимость и возвращаем верстку товара.
-// const renderPage = list => {
-//     const productsList = list.map(item => renderProduct(item)); //2.В теле функции мы применили метод map, который возвращает массив. К каждому элементу массива мы применяем функцию renderProduct. В эту функцию мы передаем название товара и стоимость товара.
-//     console.log(productsList);
-//     document.querySelector('.products').innerHTML = productsList.join(' '); //3. Метод join преобразовал элементы массива,который вернул метод map, в строки. Таким образом, убрали запятую.
-// };
+//     }
+//     changeGoods(){
 
-// renderPage(products); //1.Запускаем функцию, в которую передаем массив products. List - массив объектов.
+//     }
+//     render(){ //вывести список всех товаров
+
+//     }
+// }
+
+// class CartItem {
+//     render() { //вывести список одного товара
+        
+//     }
+// }
+
+class Cart {
+    constructor(container='.cart-block') {
+        this.container = container; 
+        this.goods = [];
+        this._getCartBlock() 
+            .then(data => { 
+                this.goods = data.contents; 
+                this.render();
+            });
+    }
+
+    _getCartBlock(){ 
+
+        return fetch(`${API}/getBasket.json`) 
+            .then(result => result.json()) 
+            .catch(error => { 
+                console.log(error);
+            })
+    }
+
+    render() { 
+        const block = document.querySelector(this.container); 
+        for(let product of this.goods) { 
+            const item = new CartItem(product); 
+            block.insertAdjacentHTML('beforeend', item.render());
+        }
+    }
+}
+
+class CartItem {
+    constructor(product, img='img/prod_1.png') { 
+        this.title = product.title;
+        this.id = product.id;
+        this.price = product.price;
+        this.img = img;
+    }
+    render() {
+        return `<div class="product-item">
+                 <h3>${this.title}</h3>
+                 <img src = ${this.img}>
+                 <p>${this.price}</p>
+                 <button class="buy-btn">Купить</button>
+             </div>`
+    };
+}
